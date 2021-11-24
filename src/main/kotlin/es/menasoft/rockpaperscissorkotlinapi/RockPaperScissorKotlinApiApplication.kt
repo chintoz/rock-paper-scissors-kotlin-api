8ef835c5b5aa7ae3e15ce76fdbc5f272f13267ed
Jacinto J. Mena Lomeña <jacintomena@gmail.com>
2021-11-24 22:51:37 +0100

    Initial commit

---
+++ src/main/kotlin/es/menasoft/rockpaperscissorkotlinapi/RockPaperScissorKotlinApiApplication.kt
@@ -0,0 +1,11 @@
+package es.menasoft.rockpaperscissorkotlinapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RockPaperScissorKotlinApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<RockPaperScissorKotlinApiApplication>(*args)
+}

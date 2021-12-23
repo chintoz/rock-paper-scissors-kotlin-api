a027b0e8e8f7741d24d5fe44f3c47e7f29ea4e60
Jacinto Jesús Mena Lomeña <jacintomena@gmail.com>
2021-12-23 23:46:31 +0100

    Changes to add Kafka implementation for Messaging.

--- src/main/kotlin/es/menasoft/rockpaperscissorkotlinapi/messaging/Messaging.kt
+++ src/main/kotlin/es/menasoft/rockpaperscissorkotlinapi/messaging/Messaging.kt
@@ -1,6 +1,14 @@
 package es.menasoft.rockpaperscissorkotlinapi.messaging
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.kafka.clients.producer.ProducerConfig.*
+import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
 import org.springframework.stereotype.Component
 
 interface Messaging {
@@ -9,9 +17,26 @@ interface Messaging {
 
 @Component
 @ConditionalOnMissingBean(value = [Messaging::class], ignored = [KafkaMessaging::class])
-class KafkaMessaging:Messaging {
+class KafkaMessaging(val kafkaTemplate: KafkaTemplate<String, String>, val objectMapper: ObjectMapper) : Messaging {
     override fun send(routing: String, message: Any) {
-        println("Publish message $message to topic $routing")
-        // TODO: Implement kafka send message
+        kafkaTemplate.send(routing, objectMapper.writeValueAsString(message))
     }
+}
+
+@Configuration
+@ConditionalOnMissingBean(value = [Messaging::class], ignored = [KafkaMessaging::class])
+class KafkaConfig {
+    @Bean
+    fun producerFactory(): ProducerFactory<String, String> =
+        DefaultKafkaProducerFactory(
+            mapOf(
+                BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
+                KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+                VALUE_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java
+            )
+        )
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, String> = KafkaTemplate(producerFactory())
+
 }
